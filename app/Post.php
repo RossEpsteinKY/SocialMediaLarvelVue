be2eb9c3b9b5ddf4e715681021b346ad4b1360e7
RossEpsteinKY <ross.epstein@hotmail.com>
2020-01-12 22:39:18 -0500

    test green and reverse scope

--- app/Post.php
+++ app/Post.php
@@ -2,11 +2,20 @@
 
 namespace App;
 
+use App\Scopes\ReverseScope;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
   protected $guarded = [];
+
+
+  public static function boot()
+  {
+    parent::boot();
+
+    static::addGlobalScope(new ReverseScope);
+  }
 
   public function user()
   {
